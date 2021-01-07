d65951c064c45feacdcbc1c1b895a0c206e666f1
s-y1127 <yooopopo5@gmail.com>
2021-01-07 15:58:50 +0900

    learn props of components

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import React, { useState } from 'react';
 const App = () => {
   const [text, setText] = useState('');
   const onChangeText = (event) => setText(event.target.value);
+  const profiles = [
+    { name: 'Taro', age: 10 },
+    { name: 'Hanako', age: 5 },
+    { name: 'noname' },
+  ];
 
   return (
     <div>
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
       <h1>{text}</h1>
       <input type="text" value={text} onChange={onChangeText} />
     </div>
   );
 };
+
+const User = (props) => {
+  const { name, age } = props;
+
+  return (
+    <div>
+      HI I am {name}, {age} years old.
+    </div>
+  );
+};
+
+User.defaultProps = { age: 1 };
 
 export default App;
